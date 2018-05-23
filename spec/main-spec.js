3eeecf9b5e2a8a08a94abc279bdfddcb2c273f66
xiallyspider <33478703+xiallyspider@users.noreply.github.com>
2018-05-23 22:10:41 +0800

    someMistake

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,7 @@
 let sinon = require("sinon");
 let main = require("../lib/main");
+let Grade = require("./grade.js");
+let Student = require("./student.js");
 
 describe('main()', () => {
 
@@ -11,5 +13,17 @@ describe('main()', () => {
 3. 退出
 请输入你的选择（1～3）：`);
     });
-
+});
+describe('student', () => {
+    it('学生基本信息应该包含：姓名、学号、民族、班级', () => {
+        let student = new Student('小敏','20180523','汉族',2)
+        let name = student.name
+        let id = student.id
+        let region = student.region
+        let klass = student.klass
+        expect(name).to.Equal('小敏');
+        expect(id).not.equal(20180523);
+        expect(region).to.Equal('汉族');
+        expect(klass).to.Equal(2);
+    });
 });
